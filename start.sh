4c6be07c4572db1d5d78fa341e2e3962ea42adb1
Marcus Rönnbäck <marcus@ronnback.tech>
2021-12-10 09:24:31 +0100

    someone misspelled gnome

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-genome-terminal -- /bin/bash -c 'source ../devel/setup.bash; roslaunch my_robot joints.launch'
-genome-terminal -- /bin/bash -c 'source ../devel/setup.bash; roslaunch my_robot_control my_robot_control.launch'
-genome-terminal -- /bin/bash -c 'source ../devel/setup.bash; cd my_robot_control/scripts/'
+gnome-terminal -- /bin/bash -c 'source ../devel/setup.bash; roslaunch my_robot joints.launch'
+gnome-terminal -- /bin/bash -c 'source ../devel/setup.bash; roslaunch my_robot_control my_robot_control.launch'
+gnome-terminal -- /bin/bash -c 'source ../devel/setup.bash; cd my_robot_control/scripts/'
