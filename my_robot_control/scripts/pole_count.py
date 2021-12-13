2e70e5527be24889ce7c52fc29f506c2223a04aa
David <davidaxelbenjaminsson@gmail.com>
2021-12-13 11:13:05 +0100

    Added possible usecase/psuedo code to pole_count script

--- my_robot_control/scripts/pole_count.py
+++ my_robot_control/scripts/pole_count.py
@@ -8,6 +8,31 @@ def callback(data):
 	# output the pole number that the robot has passed
 	print(n)
 	#read the range matrix of the laser scan sensor. The range matrix contains 720 samples distributed in the angle range of -pi/2 to pi/2. The range[360] corresponds to the distance of the obstacle in the scan angle 0, which is x direction. Therefore, when range[360] is lower than 5, the robot is passing a pole.
+	
+	'''
+	### Possible extension: 
+
+	Look at 3 angles instead of 1. Lets call them 1 2 3.
+	2 is the angle we currently use at angle 0.
+	1 would be to the left of 2.
+	3 would be to the right of 2.
+
+	This way we could adapt to directional changes of the robot.
+	if we "trigger" in order 1 2 3. We can to the counter.
+	If we "trigger" in order 3 2 1. We cant subtract from the counter.
+
+	When we want to know how many poles we've passed from our starting position we simply take the abs-value of our counter :D
+
+	### Possible extenstion:
+
+	Incorperate IMU and odometry sensors to "map" or provide further accuracy of the pole positions.
+
+	Example 
+		-> laser scan trigger (pole detection)
+		-> Where are we? -> Look at IMU (and) Odometry sensor data
+
+	'''
+
 	distance = data.ranges[360]
 	if (distance<5) & (flag):
 		n = n+1
