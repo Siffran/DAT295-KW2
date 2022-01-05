12def19a0db0017f016f3eb9cfd2c4c16b079d9a
Marcus Rönnbäck <marcus@ronnback.tech>
2022-01-05 01:17:17 +0100

    updated kill_and_start.sh script to include delay fix

--- kill_and_start.sh
+++ kill_and_start.sh
@@ -1,5 +1,5 @@
 gnome-terminal -- /bin/bash -c 'pkill -f gazebo'
 gnome-terminal -- /bin/bash -c 'source ../../devel/setup.bash; roslaunch my_robot joints.launch'
+sleep 5
 gnome-terminal -- /bin/bash -c 'source ../../devel/setup.bash; roslaunch my_robot_control my_robot_control.launch'
 gnome-terminal -- /bin/bash -c 'source ../../devel/setup.bash; cd my_robot_control/scripts/'
-
