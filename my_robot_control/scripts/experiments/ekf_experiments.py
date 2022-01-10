ecfe001c0fae78e470eedbda12fb5f72b7fab230
David <davidaxelbenjaminsson@gmail.com>
2022-01-10 12:51:37 +0100

    ekf experiments continued

--- my_robot_control/scripts/experiments/ekf_experiments.py
+++ my_robot_control/scripts/experiments/ekf_experiments.py
@@ -1,10 +1,13 @@
 from numpy.random import randn
+from filterpy.kalman import KalmanFilter
+from scipy.linalg import block_diag
+from filterpy.common import Q_discrete_white_noise
+from filterpy.stats import plot_covariance_ellipse
 
 import matplotlib.pyplot as plt
 import numpy as np
-from kf_book.book_plots import plot_measurements
 
-class PosSensor:
+class PosSensor(object):
     def __init__(self, pos=(0, 0), vel=(0, 0), noise_std=1.):
         self.vel = vel
         self.noise_std = noise_std
@@ -17,7 +20,66 @@ class PosSensor:
         return [self.pos[0] + randn() * self.noise_std,
                 self.pos[1] + randn() * self.noise_std]
 
+### Kalman Filter
+R_std = 0.35
+Q_std = 0.04
+
+def tracker1():
+    tracker = KalmanFilter(dim_x=4, dim_z=2)
+    dt = 1.0   # time step
+
+    tracker.F = np.array([[1, dt, 0,  0],
+                          [0,  1, 0,  0],
+                          [0,  0, 1, dt],
+                          [0,  0, 0,  1]])
+    tracker.u = 0.
+    tracker.H = np.array([[1/0.3048, 0, 0, 0],
+                          [0, 0, 1/0.3048, 0]])
+
+    tracker.R = np.eye(2) * R_std**2
+    q = Q_discrete_white_noise(dim=2, dt=dt, var=Q_std**2)
+    tracker.Q = block_diag(q, q)
+    tracker.x = np.array([[0, 0, 0, 0]]).T
+    tracker.P = np.eye(4) * 500.
+    return tracker
+
+# simulate robot movement
+N = 30
+sensor = PosSensor((0, 0), (2, .2), noise_std=R_std)
+
+zs = np.array([sensor.read() for _ in range(N)])
+
+# run filter
+robot_tracker = tracker1()
+mu, cov, _, _ = robot_tracker.batch_filter(zs)
+
+for x, P in zip(mu, cov):
+    # covariance of x and y
+    cov = np.array([[P[0, 0], P[2, 0]], 
+                    [P[0, 2], P[2, 2]]])
+    mean = (x[0, 0], x[2, 0])
+    plot_covariance_ellipse(mean, cov=cov, fc='g', std=3, alpha=0.5)
+    
+#plot results
+zs *= .3048 # convert to meters
+plt.plot(mu[:, 0], mu[:, 2], label="Filter")
+plt.plot(mu[:, 0], mu[:, 2], 'bo', label="Measure")
+plt.grid()
+plt.legend()
+plt.show()
+###
+
+'''
+### Simulated position and plot
 pos, vel = (4, 3), (2, 1)
 sensor = PosSensor(pos, vel, noise_std=1)
 ps = np.array([sensor.read() for _ in range(50)])
-plot_measurements(ps[:, 0], ps[:, 1])
+
+plt.plot(ps[:, 0], ps[:, 1], 'bo', label="Pos")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.grid()
+plt.legend()
+plt.show()
+###
+'''
