322523404370bf5c888f045b2dd872b869955f32
Marcus Rönnbäck <marcus@ronnback.tech>
2022-01-12 17:10:03 +0100

    Added the configuration for gps+wheel. Still need to fix the EKF for "gpspos+wheel"

--- my_robot_control/scripts/system/main_gps_wheel.py
+++ my_robot_control/scripts/system/main_gps_wheel.py
@@ -1,11 +1,12 @@
 # other software parts
+from numpy.lib.nanfunctions import _nanvar_dispatcher
 from control_robot import set_wheel_velocity
 from LocalizationEstimator import LocalizationEstimator
-from read_sensors import read_actual_pos, WheelEncoder, IMU
+from read_sensors import read_actual_pos, WheelEncoder, read_gps_pos
 
 # rospy related
 import rospy
-from sensor_msgs.msg import Imu
+from sensor_msgs.msg import NavSatFix
 import message_filters
 import gazebo_msgs.msg
 
@@ -13,18 +14,17 @@ import gazebo_msgs.msg
 import numpy as np
 from matplotlib import pyplot as plt
 
+
 wheel_encoder = WheelEncoder()
-imu = IMU()
+gps = NavSatFix()
 
 # MODIFY: add variable for your sensor, keep pattern "sensor_direction" i.e. wheel_x
 #Sys.1
-imu_x_pos, imu_y_pos, \
-    imu_x_acc, imu_y_acc, \
-    wheel_x, wheel_y, \
-    actual_x, actual_y = 0, 0, 0, 0, 0, 0, 0, 0
+wheel_x, wheel_y, actual_x, actual_y = 0, 0, 0, 0
+gps_x = 0.0
+gps_y = 0.0
 
-
-def callback(imu_data, wheel_data, actual_data):
+def callback(gps_data, wheel_data, actual_data):
     """
     Executes the sensor functions to get current values.
     Called by the TimeSynchonizer.
@@ -32,10 +32,8 @@ def callback(imu_data, wheel_data, actual_data):
     """
 
     # MODIFY: Add get function of each sensor in the configuration.
-    global imu_x_acc, imu_y_acc, wheel_x, wheel_y, actual_x, actual_y, imu_x_pos, imu_y_pos
-
-    imu.update(imu_data, rospy.get_time())
-    imu_x_pos, imu_y_pos = imu.get_pos()
+    global gps_x, gps_y, wheel_x, wheel_y, actual_x, actual_y
+    gps_x, gps_y = read_gps_pos(gps_data)
     wheel_encoder.update(wheel_data)
     wheel_x, wheel_y = wheel_encoder.get_pos()
     actual_x, actual_y = read_actual_pos(actual_data)
@@ -64,16 +62,16 @@ if __name__ == "__main__":
     # MODIFY: Subscribe all needed sensors
     #         Add them to the ApproximateTimeSynchronizer
     #         Adjust time intervall in which two values belong to each other (Third value in ApproximateTimeSynchronizer)
-    imu_sub = message_filters.Subscriber('/imu', Imu)
+    gps_sub = message_filters.Subscriber('/gps', NavSatFix)
     wheel_sub = message_filters.Subscriber('/gazebo/link_states', gazebo_msgs.msg.LinkStates)
     actual_sub = message_filters.Subscriber('/gazebo/model_states', gazebo_msgs.msg.ModelStates)
 
-    ts = message_filters.ApproximateTimeSynchronizer([imu_sub, wheel_sub, actual_sub], 10, 0.02, True)
+    ts = message_filters.ApproximateTimeSynchronizer([gps_sub, wheel_sub, actual_sub], 10, 0.02, True)
     ts.registerCallback(callback)
 
     # MODIFY: Create instance of Localization estimater.
     #         The setup function has to be created in the LocalizationEstimator.py file
-    loc_est = LocalizationEstimator("imupos+wheel", RATE_Hz)
+    loc_est = LocalizationEstimator("gpspos+wheel", RATE_Hz)
 
     # MODIFY: Amount of steps the while loop records values.
     steps = 200
@@ -85,18 +83,18 @@ if __name__ == "__main__":
         "EKF y":[],
         "Wheel x":[],
         "Wheel y":[],
-        "IMU x":[],
-        "IMU y":[],
+        "GPS x":[],
+        "GPS y":[],       
         "Actual x":[],
         "Actual y":[]
     }
 
-    imu.set_prev_time(rospy.get_time())
+    #imu.set_prev_time(rospy.get_time())
 
     while i <= steps:
 
         # MODIFY: Measurement vector with your sensor values
-        z = np.array([wheel_x, wheel_y, imu_x_pos, imu_y_pos])
+        z = np.array([wheel_x, wheel_y, gps_x, gps_y])
 
         loc_est.update(z)
         loc_est.predict()
@@ -107,8 +105,8 @@ if __name__ == "__main__":
         recorded_positions["EKF y"].append(state_vector[3])
         recorded_positions["Wheel x"].append(wheel_x)
         recorded_positions["Wheel y"].append(wheel_y)
-        recorded_positions["IMU x"].append(imu_x_pos)
-        recorded_positions["IMU y"].append(imu_y_pos)
+        recorded_positions["GPS x"].append(gps_x)
+        recorded_positions["GPS y"].append(gps_y)
         recorded_positions["Actual x"].append(actual_x)
         recorded_positions["Actual y"].append(actual_y)
 
@@ -121,12 +119,39 @@ if __name__ == "__main__":
     # MODIFY: Values to plot.
     plt.plot(recorded_positions["EKF x"], recorded_positions["EKF y"], label="EKF pos")
     plt.plot(recorded_positions["Wheel x"], recorded_positions["Wheel y"], label="Wheel pos")
-    plt.plot(recorded_positions["IMU x"], recorded_positions["IMU y"], label="IMU pos")
+    plt.plot(recorded_positions["GPS x"], recorded_positions["GPS y"], label="GPS pos")
     plt.plot(recorded_positions["Actual x"], recorded_positions["Actual y"], label="Actual pos")
     plt.xlabel("x")
     plt.ylabel("y")
     plt.title("EKF values")
     plt.legend()
     plt.show()
+
+    # the error between the estimated position 
+    fig2 = plt.figure()
+    plt.subplot(1, 2, 1) # row 1, col 2 index 1
+    c1 = [recorded_positions["EKF y"][i] - recorded_positions["Actual y"][i] for i in range(len(recorded_positions["EKF y"]))]
+    c2 = [recorded_positions["GPS y"][i] - recorded_positions["Actual y"][i] for i in range(len(recorded_positions["GPS y"]))]
+    c3 = [recorded_positions["Wheel y"][i] - recorded_positions["Actual y"][i] for i in range(len(recorded_positions["Wheel y"]))]
+    plt.plot(range(0,steps+1), c1, color="black", label="Actual pos")
+    plt.plot(range(0,steps+1), c2, color="black", label="Actual pos")
+    plt.plot(range(0,steps+1), c3, color="black", label="Actual pos")
+    plt.title("the error for y")
+    plt.xlabel('steps ')
+    plt.ylabel('Y-Distance ')
+    plt.legend()
+
+    plt.subplot(1, 2, 2) # index 2
+    cc1 = [recorded_positions["EKF x"][i] - recorded_positions["Actual x"][i] for i in range(len(recorded_positions["EKF x"]))]
+    cc2 = [recorded_positions["GPS x"][i] - recorded_positions["Actual x"][i] for i in range(len(recorded_positions["GPS x"]))]
+    cc3 = [recorded_positions["Wheel x"][i] - recorded_positions["Actual x"][i] for i in range(len(recorded_positions["Wheel x"]))]
+    plt.plot(range(0,steps+1), cc1, color="black", label="Actual pos")
+    plt.plot(range(0,steps+1), cc2, color="black", label="Actual pos")
+    plt.plot(range(0,steps+1), cc3, color="black", label="Actual pos")
+    plt.title("the error for x")
+    plt.xlabel('steps ')
+    plt.ylabel('X-Distance ')
+
+    plt.show()
+
         
-    
